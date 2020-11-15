228af44d7f7b265c01d16cd64d7949e195a683bb
Yuto Watanabe <yuto.w51942@gmail.com>
2020-11-15 21:19:03 +0900

    change ga id

--- with-google-analytics-app/lib/gtag.js
+++ with-google-analytics-app/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'G-CRDVYTD170'
+export const GA_TRACKING_ID = 'G-X0Y5NTS4M9'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
